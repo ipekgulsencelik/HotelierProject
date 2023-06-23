2c55bd156f2bc9fb386c9a8edab344f3c8b3b57d
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-23 14:32:44 +0300

    Abstract Interfaces

---
+++ Consume/HotelierProject.DataAccessLayer/Abstract/IGenericDAL.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelierProject.DataAccessLayer.Abstract
+{
+    public interface IGenericDAL<T> where T : class
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetList();
+        T GetByID(int id);
+    }
+}
---
+++ Consume/HotelierProject.DataAccessLayer/Abstract/ISubscribeDAL.cs
@@ -0,0 +1,8 @@
+﻿using HotelierProject.EntityLayer.Concrete;
+
+namespace HotelierProject.DataAccessLayer.Abstract
+{
+    public interface ISubscribeDAL : IGenericDAL<Subscribe>
+    {
+    }
+}
