25609bfc97fb80e39e06173567e8d6927928a753
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-23 14:23:01 +0300

    create entities

---
+++ Consume/HotelierProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,19 @@
+﻿using HotelierProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelierProject.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI;database=HotelierDB;integrated security=true;trusted_connection=true;encrypt=false");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
---
+++ Consume/HotelierProject.EntityLayer/Concrete/Subscribe.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelierProject.EntityLayer.Concrete
+{
+    public class Subscribe
+    {
+        public int SubscribeID { get; set; }
+        public string? Mail { get; set; }
+    }
+}
