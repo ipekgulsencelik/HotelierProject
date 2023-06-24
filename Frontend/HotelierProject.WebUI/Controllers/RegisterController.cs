09eb772ad8adc773bf74648bc7c735b735bcd0e2
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-25 01:58:24 +0300

    register

---
+++ Frontend/HotelierProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using HotelierProject.EntityLayer.Concrete;
+using HotelierProject.WebUI.DTOs.RegisterDTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelierProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateNewUserDTO createNewUserDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var appUser = new AppUser()
+            {
+                Name = createNewUserDTO.Name,
+                Email = createNewUserDTO.Mail,
+                Surname = createNewUserDTO.Surname,
+                UserName = createNewUserDTO.Username
+            };
+
+            var result = await _userManager.CreateAsync(appUser, createNewUserDTO.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            return View();
+        }
+    }
+}
