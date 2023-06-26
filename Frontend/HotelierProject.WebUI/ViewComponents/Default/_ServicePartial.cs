72365cf90b7f4499cd06fba8eb6b385fe1d192d6
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-26 20:25:51 +0300

    default service

---
+++ Frontend/HotelierProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -0,0 +1,31 @@
+﻿using HotelierProject.WebUI.DTOs.ServiceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelierProject.WebUI.ViewComponents.Default
+{
+    public class _ServicePartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7126/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
