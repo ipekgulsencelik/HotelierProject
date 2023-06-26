9a6f0ca36d42423da27f99eb9e8257bad522eeea
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-26 20:38:33 +0300

    default partial views

---
+++ Frontend/HotelierProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -0,0 +1,31 @@
+﻿using HotelierProject.WebUI.DTOs.RoomDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelierProject.WebUI.ViewComponents.Default
+{
+    public class _OurRoomsPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7126/api/Room");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDTO>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelierProject.WebUI/ViewComponents/Default/_SpinnerPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelierProject.WebUI.ViewComponents.Default
+{
+    public class _SpinnerPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
