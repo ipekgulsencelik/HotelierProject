ba3d5aeea966a981f60d4a6d63a7726f7ffcc339
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-26 20:23:29 +0300

    default testimonial

---
+++ Frontend/HotelierProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -0,0 +1,31 @@
+﻿using HotelierProject.WebUI.DTOs.TestimonialDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelierProject.WebUI.ViewComponents.Default
+{
+    public class _TestimonialPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7126/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDTO>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
