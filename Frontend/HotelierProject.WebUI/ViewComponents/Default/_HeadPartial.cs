0cab2301f765dd68ffff5826c78e0a06f81b7de4
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-26 20:24:03 +0300

    default head

---
+++ Frontend/HotelierProject.WebUI/ViewComponents/Default/_HeadPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelierProject.WebUI.ViewComponents.Default
+{
+    public class _HeadPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
