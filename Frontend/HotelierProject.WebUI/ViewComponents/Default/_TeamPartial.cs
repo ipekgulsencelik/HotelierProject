239ca02b07b5b001ca2571824bf4e0750eeda81b
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-26 20:21:11 +0300

    default team

---
+++ Frontend/HotelierProject.WebUI/ViewComponents/Default/_TeamPartial.cs
@@ -0,0 +1,31 @@
+﻿using HotelierProject.WebUI.DTOs.StaffDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelierProject.WebUI.ViewComponents.Default
+{
+    public class _TeamPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TeamPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7126/api/Staff");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultStaffDTO>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
