18bfe8db34256833fa2b0a5678147d950b45628f
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-26 20:19:02 +0300

    default footer

---
+++ Frontend/HotelierProject.WebUI/ViewComponents/Default/_FooterPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelierProject.WebUI.ViewComponents.Default
+{
+    public class _FooterPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
