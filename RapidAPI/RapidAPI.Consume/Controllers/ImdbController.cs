a52754b3f74efb1c975f266b56c47fd0150193aa
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-07-03 00:20:12 +0300

    IMDB API

---
+++ RapidAPI/RapidAPI.Consume/Controllers/ImdbController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidAPI.Consume.Models;
+
+namespace RapidAPI.Consume.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<MovieApiViewModel> movieApiViewModels = new List<MovieApiViewModel>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "6b5d9ea256msh2120b0d66e6f078p16642ejsn95a51d4014f3" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                movieApiViewModels = JsonConvert.DeserializeObject<List<MovieApiViewModel>>(body);
+                return View(movieApiViewModels);
+            }
+        }
+    }
+}
---
+++ RapidAPI/RapidAPI.Consume/Models/MovieApiViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RapidAPI.Consume.Models
+{
+    public class MovieApiViewModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+        public string rating { get; set; }
+        public string trailer { get; set; }
+    }
+}
