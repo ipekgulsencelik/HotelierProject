60b67e0f9f141c8409c7bccf16383d23fc0c82ab
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-07-03 17:43:35 +0300

    Searc hLocation ID API

---
+++ RapidAPI/RapidAPI.Consume/Controllers/SearchLocationIDController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidAPI.Consume.Models;
+
+namespace RapidAPI.Consume.Controllers
+{
+    public class SearchLocationIDController : Controller
+    {
+        public async Task<IActionResult> Index(string cityName)
+        {
+            if (!string.IsNullOrEmpty(cityName))
+            {
+                List<BookingApiLocationSearchViewModel> model = new List<BookingApiLocationSearchViewModel>();
+
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={cityName}&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "6b5d9ea256msh2120b0d66e6f078p16642ejsn95a51d4014f3" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<List<BookingApiLocationSearchViewModel>>(body);
+                    return View(model.Take(1).ToList());
+                }
+            }
+            else
+            {
+                List<BookingApiLocationSearchViewModel> model = new List<BookingApiLocationSearchViewModel>();
+
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=paris&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "6b5d9ea256msh2120b0d66e6f078p16642ejsn95a51d4014f3" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<List<BookingApiLocationSearchViewModel>>(body);
+                    return View(model.Take(1).ToList());
+                }
+            }
+
+        }
+    }
+}
