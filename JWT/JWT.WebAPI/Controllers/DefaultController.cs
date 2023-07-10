b7969bd6cee7c91e73064c05dd7144598ecdea60
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-07-11 01:07:24 +0300

    Json Web Token

---
+++ JWT/JWT.WebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using JWT.WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult AddToken()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult CreateAdminToken()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test()
+        {
+            return Ok("Welcome");
+        }
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult TestToken()
+        {
+            return Ok("Token Başarılı Bir Şekilde Giriş Yaptı");
+        }
+    }
+}
